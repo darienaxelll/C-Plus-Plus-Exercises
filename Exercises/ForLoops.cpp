0452d6769c29142f611b4b7f35fec39bc344a47f
Darien Salazar <darien.axell.salazar@gmail.com>
2022-11-21 22:10:32 -0600

    Going through striongs with  loops

---
+++ Exercises/ForLoops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int numVals;
+
+   for (int i = 0; i < numVals; i++) {
+    cin >> numVals;
+    cout << numVals << " things."  << endl;
+   }
+
+   return 0;
+}
---
+++ Exercises/PasswordReq.cpp
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+int main() {
+   string codeStr;
+   unsigned int i;
+   
+   cin >> codeStr;
+
+   
+
+   cout << "New password: " << codeStr << endl;
+   
+   return 0;
+}
