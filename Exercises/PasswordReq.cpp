e6a80fb6bde168ab8714efb608dd858c155de018
Darien Salazar <darien.axell.salazar@gmail.com>
2022-11-22 22:49:51 -0600

    Working on password exercise

--- Exercises/PasswordReq.cpp
+++ Exercises/PasswordReq.cpp
@@ -7,9 +7,38 @@ int main() {
    
    cin >> codeStr;
 
+   while (codeStr != string::npos)
+   {
+    codeStr.replace(codeStr, i , "x");
+   }
    
 
    cout << "New password: " << codeStr << endl;
    
+   return 0;
+}
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string userText;
+   int usaIndex;
+
+   cout << "Enter text: ";
+   getline(cin, userText);
+
+   // At least one occurrence exists
+   while (userText.find("U.S.A.") != string::npos) {
+      // Get index of first instance
+      usaIndex = userText.find("U.S.A.");
+
+      // U.S.A. is 6 long
+      userText.replace(usaIndex, 6, "USA");
+   }
+
+   cout << "New text: " << userText<< endl;
+
    return 0;
 }
