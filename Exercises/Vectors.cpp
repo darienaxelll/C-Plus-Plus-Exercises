beb7d9950922e388a0b3050353422162697c6238
Darien Salazar <darien.axell.salazar@gmail.com>
2022-11-27 12:55:18 -0600

    Started working with vectors

---
+++ Exercises/Vectors.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+   const int NUM_VALS = 5;
+   vector<int> runTimes(NUM_VALS);
+   unsigned int i;
+
+   // Populate vector
+   for (i = 0; i < runTimes.size(); ++i) {
+      cin >> runTimes.at(i);
+   }
+
+   /* Your solution goes here  */
+   for (i = 0; i < 3; i++)
+   {
+    cout << runTimes.at(i) << endl;
+   }
+   
+
+   return 0;
+}
